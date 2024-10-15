bb5abbade697546981dc7d41814376a596464298
Enzofdino <enzofhostin@gmail.com>
2024-10-15 11:36:35 -0300

    acrescentando metodo spawnfood para comida
    e futuramente colocarei outro metodo para restaurar o spawn das maças

--- Cobra enzo/Assets/Scripts/Comida.cs
+++ Cobra enzo/Assets/Scripts/Comida.cs
@@ -4,15 +4,61 @@ using UnityEngine;
 
 public class Comida : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Transform foodPrefab;
+    public Cobra cobra;
+
+    private Transform currentFood;
+
     void Start()
     {
-        
+        SpawnFood();
     }
 
-    // Update is called once per frame
-    void Update()
+    void SpawnFood()
     {
+       
+        float width = Cobra.cobra.pegartamanho();
+        float height = Cobra.cobra.pegarcomprimento();
+
+        Vector2 randomPosition;
+
         
+        do
+        {
+            float x = Random.Range(-width / 2 + Cobra.cobra.tamanhocélula / 2, width / 2 - Cobra.cobra.tamanhocélula / 2);
+            float y = Random.Range(-height / 2 + Cobra.cobra.tamanhocélula / 2, height / 2 - Cobra.cobra.tamanhocélula / 2);
+            randomPosition = new Vector2(x, y);
+        }
+        while (posiçãoéocupada(randomPosition));
+
+       
+        if (currentFood != null)
+        {
+            Destroy(currentFood.gameObject);
+        }
+
+        
+        currentFood = Instantiate(foodPrefab, randomPosition, Quaternion.identity).transform;
     }
+
+   
+    bool posiçãoéocupada(Vector2 position)
+    {
+       
+        if ((Vector2)Cobra.cobra.transform.position == position)
+        {
+            return true;
+        }
+
+       
+        foreach (Transform segment in Cobra.cobra.body)
+        {
+            if ((Vector2)segment.position == position)
+            {
+                return true; 
+            }
+        }
+        return false;
+    }
+
 }
